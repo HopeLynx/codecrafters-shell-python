865592e821fec6ad66e8a112b8709621443a6688
Grigorii <hopelynxstudio@gmail.com>
2024-06-08 16:25:12 +0800

    stage 3_4

--- app/main.py
+++ app/main.py
@@ -2,16 +2,15 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
     command_list = []
     # Wait for user input
-    command = input()
-
-    while command:
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        command = input()
         if command not in command_list:
             sys.stdout.write(f"{command}: command not found\n")
-        command = input()
+            
 
 if __name__ == "__main__":
     main()
