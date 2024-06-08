dba343ff954e26963c84d66455c7b4a71d79194a
Grigorii <hopelynxstudio@gmail.com>
2024-06-08 16:33:48 +0800

    stage 5

--- app/main.py
+++ app/main.py
@@ -2,16 +2,21 @@ import sys
 
 
 def main():
-    command_list = ["exit 0"]
+    command_list = ["exit","echo"]
     # Wait for user input
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
-        if command not in command_list:
-            sys.stdout.write(f"{command}: command not found\n")
-        elif command == command_list[0]:
+        splitted_command = command.split()
+        if splitted_command[0] not in command_list:
+            sys.stdout.write(f"{splitted_command[0]}: command not found\n")
+        elif splitted_command[0] == command_list[0]:
             return 0
+        elif splitted_command[0] == command_list[1]:
+            for word in splitted_command[1:]:
+                sys.stdout.write(f"{word}")
+            sys.stdout.write(f"\n")
             
 
 if __name__ == "__main__":
